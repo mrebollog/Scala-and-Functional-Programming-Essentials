9fff6dc6892d4bce8810f805ea25093329cf940b
mrebollog <mrebollo@griddynamics.com>
2025-11-10 11:32:33 -0300

    Finished MethodNotation tasks

--- src/main/scala/lectures/part2oop/OOBasics.scala
+++ src/main/scala/lectures/part2oop/OOBasics.scala
@@ -2,6 +2,45 @@ package lectures.part2oop
 
 object OOBasics extends App {
 
+  /*
+    Novel and a Writer
+
+    Writer: first name, surname, year of birth
+      - method fullname
+
+    Novel: name, year of release, author
+    - authorAge (at the yer of release)
+    - isWrittenBy(author)
+    - copy (new year of release) = new instance of Novel
+   */
+
+  class Writer(firstName: String, surname: String, val year: Int) {
+    def fullname: String = firstName + " " + surname
+  }
+
+  class Novel(name: String, yearOfRelease: Int, author: Writer) {
+    def authorAge: Int = yearOfRelease - author.year
+    def isWrittenBy(otherAutor: Writer): Boolean = author == otherAutor
+    def copy(newYearOfRelease: Int): Novel = new Novel(name, newYearOfRelease, author)
+  }
+
+  /*
+    Counter class
+      - receives an int value
+      - method current count
+      - method to increment/decrement => new Counter
+      - overload inc/dec to receive an amount
+   */
+
+  class Counter(value: Int) {
+    def currentCount: Int = value
+    def increment: Counter = new Counter(value + 1)
+    def decrement: Counter = new Counter(value - 1)
+    def increment(amount: Int) = new Counter(value + amount)
+    def decrement(amount: Int) = new Counter(value - amount)
+  }
+
+
   private val author = new Writer("Matias", "Rebollo", 2003)
   private val imposter = new Writer("Matias", "Rebollo", 2003)
   private val novel = new Novel("Yo, Matias", 2018, author)
@@ -19,43 +58,5 @@ object OOBasics extends App {
 
 
 
-}
-  /*
-    Novel and a Writer
-
-    Writer: first name, surname, year of birth
-      - method fullname
-
-    Novel: name, year of release, author
-    - authorAge (at the yer of release)
-    - isWrittenBy(author)
-    - copy (new year of release) = new instance of Novel
-   */
-
-class Writer(firstName: String, surname: String, val year: Int) {
-  def fullname: String = firstName + " " + surname
-}
-
-class Novel(name: String, yearOfRelease: Int, author: Writer) {
-  def authorAge: Int = yearOfRelease - author.year
-  def isWrittenBy(otherAutor: Writer): Boolean = author == otherAutor
-  def copy (newYearOfRelease: Int): Novel = new Novel(name, newYearOfRelease, author)
-}
-
-
-  /*
-    Counter class
-      - receives an int value
-      - method current count
-      - method to increment/decrement => new Counter
-      - overload inc/dec to receive an amount
-   */
-
-class Counter(value: Int) {
-  def currentCount: Int = value
-  def increment: Counter = new Counter(value + 1)
-  def decrement: Counter = new Counter(value - 1)
-  def increment(amount: Int) = new Counter(value + amount)
-  def decrement(amount: Int) = new Counter(value - amount)
 }
 
