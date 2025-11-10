e6fea7ba215ce41434a8cfd654208ff4242e80ba
mrebollog <mrebollo@griddynamics.com>
2025-11-10 12:31:45 -0300

    Finished Generics tasks

--- src/main/scala/exercises/MyList.scala
+++ src/main/scala/exercises/MyList.scala
@@ -1,6 +1,6 @@
 package exercises
 
-abstract class MyList {
+abstract class MyList[+A] {
   /*
      head = first element of  the  list
      tail = remainder of the list
@@ -8,27 +8,29 @@ abstract class MyList {
      add(int) => new list with this element added
      toString => a string representation of the list
    */
-  def head: Int
-  def tail: MyList
+
+  // expand MyList to be generic
+  def head: A
+  def tail: MyList[A]
   def isEmpty: Boolean
-  def add(value: Int): MyList
+  def add[B >: A](value: B): MyList[B]
   def printElements: String
   override def toString: String = "[" + printElements + "]"
 }
 
-object EmptyList extends MyList{
-  def head: Int = throw new NoSuchElementException()
-  def tail: MyList = throw new NoSuchElementException()
+object EmptyList extends MyList[Nothing] {
+  def head: Nothing = throw new NoSuchElementException()
+  def tail: MyList[Nothing] = throw new NoSuchElementException()
   def isEmpty: Boolean = true
-  def add(value: Int): MyList = new NotEmptyList(value, EmptyList)
+  def add[B >: Nothing](value: B): MyList[B] = new NotEmptyList(value, EmptyList)
   def printElements: String = ""
 }
 
-class NotEmptyList(h: Int, t: MyList) extends MyList{
-  def head: Int = h
-  def tail: MyList = t
+class NotEmptyList[+A](h: A, t: MyList[A]) extends MyList[A]{
+  def head: A = h
+  def tail: MyList[A] = t
   def isEmpty: Boolean = false
-  def add(value: Int): MyList = new NotEmptyList(value, this)
+  def add[B >: A](value: B): MyList[B] = new NotEmptyList(value, this)
   def printElements: String =
     if (t.isEmpty) "" + h
     else s"$h ${t.printElements}"
